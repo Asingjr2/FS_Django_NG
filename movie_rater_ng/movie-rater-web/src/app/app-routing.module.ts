d9e1c57c89f74b85baa84368da343e4036054779
asingjr2 <asingjr2@gmail.com>
2018-05-10 00:26:09 -0400

    adding app-routing module and initialized components for home and register

--- movie_rater_ng/movie-rater-web/src/app/app-routing.module.ts
+++ movie_rater_ng/movie-rater-web/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+
+// Creating array of routes for easy use and export later
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "home", component: HomeComponent },
+  { path:"", redirectTo: "login/", pathMatch: "full"},
+]
+
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes),
   ],
-  declarations: []
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- movie_rater_ng/movie-rater-web/src/app/app.module.ts
+++ movie_rater_ng/movie-rater-web/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,

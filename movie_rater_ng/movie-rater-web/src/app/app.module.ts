3cb571e55779cc01c177f5587f002ab8e2902f4b
asingjr2 <asingjr2@gmail.com>
2018-05-10 23:57:24 -0400

    added user service and mae changes to env file.  trying django api

--- movie_rater_ng/movie-rater-web/src/app/app.module.ts
+++ movie_rater_ng/movie-rater-web/src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { HttpClientModule } from "@angular/common/http";
 
+import { MatButtonModule} from "@angular/material";
+import {  MatFormFieldModule } from "@angular/material/form-field";
+import {  MatInputModule } from "@angular/material/input";
+import {  MatIconModule } from "@angular/material/icon";
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -20,7 +27,14 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    HttpModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- movie_rater_ng/movie-rater-web/src/app/login/login.component.ts
+++ movie_rater_ng/movie-rater-web/src/app/login/login.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+
+import { UserService } from "../user.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userLogin : FormGroup;
+  loading : boolean;
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private userService: UserService
+  ) {
+    this.loading = false,
+    this.userLogin = this.fb.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required],
+    });
+
+  }
 
   ngOnInit() {
   }
 
+  onLogin() {
+    this.loading = true;
+    this.userService.loginUser( this.userLogin.value).subscribe(
+      response => {
+        this.loading = false;
+        console.log("response", response);
+      },
+      error => {
+        this.loading = false;
+        console.log("error", error);
+      }
+    )
+    console.log(this.userLogin.value);
+  }
+
+  // goToHome(){
+  //   this.router.navigate(["home"]);
+  // }
 }
